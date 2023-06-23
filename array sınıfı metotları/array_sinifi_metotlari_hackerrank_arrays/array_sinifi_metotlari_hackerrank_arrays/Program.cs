5ea10fe18ccbf91a3461984f6c44f5a19fffa850
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2023-06-23 23:24:17 +0300

    Add files via upload

---
+++ array sınıfı metotları/array_sinifi_metotlari_hackerrank_arrays/array_sinifi_metotlari_hackerrank_arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        string[] durruk = Console.ReadLine().Split(' ');
+        int[] sayiDizisi = Array.ConvertAll(durruk, Int32.Parse);
+        Array.Reverse(sayiDizisi);
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.Write($"{sayi} ");
+        }
+    }
+}
---
+++ array sınıfı metotları/array_sinifi_metotlari_hackerrank_birthday/array_sinifi_metotlari_hackerrank_birthday/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] mumlar = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int maxHeight = 0;
+        int maxHeightCandles = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (mumlar[i] > maxHeight)
+            {
+                maxHeight = mumlar[i];
+                maxHeightCandles = 1;
+            }
+            else if (mumlar[i] == maxHeight)
+            {
+                maxHeightCandles++;
+            }
+        }
+
+        Console.WriteLine(maxHeightCandles);
+    }
+}
